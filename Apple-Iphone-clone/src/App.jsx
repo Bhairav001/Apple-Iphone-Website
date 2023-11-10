1d2e589e9e70ce11334f7ee2f42923932de4c84c
Bhairav <bhairavgotam066@gmail.com>
2023-11-10 16:40:21 +0530

    added CameraSection

--- Apple-Iphone-clone/src/App.jsx
+++ Apple-Iphone-clone/src/App.jsx
@@ -1,8 +1,15 @@
 
 import './App.css'
+import { ColorContextProvider } from './context/ColorContext'
 import BatterySection from './sections/BatterySection'
+import CameraSection from './sections/CameraSection'
+import ColorSection from './sections/ColorSection'
 import DesignSection from './sections/DesignSection'
+import DisplaySection from './sections/DisplaySection'
 import HeroSection from './sections/HeroSection'
+import PhoneModel from './sections/PhoneModel'
+import PricingSection from './sections/PricingSection'
+import ProcessorSection from './sections/ProcessorSection'
 import Quote from './sections/Quote'
 import { GlobalStyle } from './styles/GlobalStyle'
 
@@ -11,10 +18,18 @@ function App() {
   return (
     <>
       <GlobalStyle/>
-      <BatterySection/>
+      <PhoneModel/>
       <HeroSection/>
       <DesignSection/>
       <Quote/>
+      <DisplaySection/>
+      <ProcessorSection/>
+      <BatterySection/>
+      <ColorContextProvider>
+         <ColorSection/>
+         <CameraSection/>
+         <PricingSection/>
+      </ColorContextProvider>
     </>
   )
 }
